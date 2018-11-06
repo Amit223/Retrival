a7b1986878c1b5338720b39250f7fa6da3b7c5f2
liad <liadbercovich@gmail.com>
2018-11-06 18:57:38 +0200

    added stemmer

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Dictionary;
+import java.util.Vector;
+
 public class Parser {
 
+    Dictionary<String,Integer> termList;
     //this class is for parsing all the text.
+   public void Parse(String doc){
+       Vector<String> ListOfTokens= new Vector <String> (Arrays.asList(doc.split(" "))); //use vector beacause: https://stackoverflow.com/questions/11001330/java-split-string-performances
+        PorterStemmer stemmer=new PorterStemmer();
+        
+   }
 
+    public Dictionary<String, Integer> getTermList() {
+        return termList;
+    }
 }
