b2202dca371464a013dc891813765d1a49f6e2c4
liad <liadbercovich@gmail.com>
2018-11-06 22:02:25 +0200

    added stop words+ start work on parse objects-added number

---
+++ src/main/java/ParseObjects/Number.java
@@ -0,0 +1,13 @@
+package ParseObjects;
+
+import java.util.Vector;
+
+public class Number {
+    public static Vector<String> Parse(String number){
+        Vector<String> out=new Vector<String>();
+
+
+
+        return out;
+    }
+}
