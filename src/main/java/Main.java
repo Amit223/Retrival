4b8e5ad2adcfd40828c4ddd5a2a96e9370d72921
AMIT MOSHE <amit.moshe100@gmail.com>
2018-11-30 15:46:39 +0200

    fixed bug in Between,Number added function in readfile- readStopWords + now indexer can get any city- not only capital

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,42 @@
+import ParseObjects.Between;
 import ParseObjects.Distances;
+import ParseObjects.Number;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
 import java.io.RandomAccessFile;
-import java.util.Dictionary;
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Vector;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
        // Test2();
+       // Test();
+       // Test3();
+
+        Test4();
         
     }
 
+    private static void Test4() {
+        Indexer indexer=new Indexer();
+        String details=indexer.getCityDetails("los");
+        String[] strings=details.split("-");
+        System.out.println(Arrays.toString(strings));
+    }
+
+    private static void Test3() {
+        ReadFile readFile=new ReadFile();
+        String s=readFile.readStopWords("C:\\Users\\AMIT MOSHE\\Desktop\\אוניברסיטה\\סמסטר ה\\אחזור\\test");
+        System.out.println(s);
+    }
+
+    private static void Test() {
+        Vector <String> s= Between.Parse("13 million-12 million");
+        System.out.println(s.toString());
+    }
 
 
     public static void Test2(){
