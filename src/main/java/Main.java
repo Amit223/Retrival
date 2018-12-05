090e99c0de966439ccc0638e1040db3fa5ee629b
amit <amit.moshe100@gmail.com>
2018-12-05 17:40:30 +0200

    fixed city dictionary

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,12 +9,16 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
     public static void main(String[] args) {
-        String s = ReadFile.readStopWords("C:\\Users\\liadber\\Downloads\\אחזור");
-        StopWords.setStopwords(s);
-        Parser p = new Parser();
-        p.Parse("12 bn u.s. dollars", true, "Paris");
-        p.printTermList();
-
+       Vector<String> vector=new Vector<>();
+        vector.add("a");
+        vector.add("b");
+        vector.add("c");
+        String s=vector.toString();
+        String[] values=s.split(",");
+        vector=new Vector();
+        vector.add(values[0].substring(1,values[0].length()));//the country
+        vector.add(values[1]);//the coin
+        vector.add(values[2].substring(0,values[2].length()-1));//population
     }
 }
 
