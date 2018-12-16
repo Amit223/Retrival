8137eb0cf0a2356df8488f0bbae10a428aff7789
liadber <liadbercovich@gmail.com>
2018-12-16 21:35:15 +0200

    build the searcher outline, and open class for ranker

--- src/main/java/Ranker.java
+++ src/main/java/Ranker.java
@@ -4,9 +4,9 @@ import java.util.Vector;
 
 public class Ranker {
 
-
+    //todo priority queue documents grade of 50
     /**
-     * this function give a grade to any doc and add it to priority queue of.
+     * this function give a grade to any doc and add it to priority queue of 50.
      * @param term_tfVector - vector of term_tf
      */
     public void Rank(Vector<Pair<String, Integer>> term_tfVector) {
